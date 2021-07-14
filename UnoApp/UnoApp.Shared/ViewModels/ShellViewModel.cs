ad01ab75e2a2933e75a86f41d66af155776ca070
BenDerPan <BenDerPan>
2021-07-14 19:55:46 +0800

    init

---
+++ UnoApp/UnoApp.Shared/ViewModels/ShellViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using UnoApp.Core.Mvvm;
+
+namespace UnoApp.Shared.ViewModels
+{
+    public class ShellViewModel : ViewModelBase
+    {
+        public ShellViewModel()
+        {
+            Title = "Main Page";
+        }
+    }
+}
---
+++ WPFApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Prism.Mvvm;
+using Prism.Regions;
+using WPFApp.Views;
+
+namespace WPFApp.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        readonly IRegionManager _regionManager;
+        private string _title = "Prism Application";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainWindowViewModel(IRegionManager regionManager)
+        {
+            this._regionManager = regionManager;
+            _regionManager.RegisterViewWithRegion("ContentRegion", typeof(LoginView));
+        }
+
+    }
+}
